eb225fe23a68c8ac2efc9124934edeaf93feb02d
Denzell Grant <denzgrant@gmail.com>
2020-07-07 22:41:20 -0700

    updated icons

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,7 +12,7 @@ const FILES_TO_CACHE = [
     "/assets/images/icons/icon-152x152.png",
     "/assets/images/icons/icon-192x192.png",
     "/assets/images/icons/icon-384x384.png",
-    "/assets/images/icons/icon-512x512.png",
+    "/assets/images/icons/icon-512x512.png"
 
 ];
 
